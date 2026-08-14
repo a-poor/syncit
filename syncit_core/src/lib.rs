3ac293f788242cf69a6dda7f4254e13d4c3b4737
Austin Poor <45295232+a-poor@users.noreply.github.com>
2026-08-14 07:54:03 -0700

    Starting on the server

--- syncit_core/src/lib.rs
+++ syncit_core/src/lib.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use autosurgeon::{Counter, Hydrate, Reconcile, Text};
+
+#[derive(Debug, Clone, Hydrate, Reconcile)]
+pub struct SyncItDoc {
+    pub name: String,
+    pub active: bool,
+    pub count: Counter,
+    pub desc: Text,
 }
